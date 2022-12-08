393db94d8461df9b4d9fbbe47ff2d1504f9d9695
azfoxxo <knotgamestudio@gmail.com>
2022-12-08 21:55:52 +0000

    Intergrated Paws logger into Heroes

--- heroes/Attributes/AutoInitialise.cs
+++
@@ -1,5 +0,0 @@
-namespace Heroes;
-
-/// <summary> Automatically initialise a hero on engine start. </summary>
-[AttributeUsage(AttributeTargets.Class)]
-public class AutoInitialiseAttribute : Attribute {}
