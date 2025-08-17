3858903a3738ce01e30f0878ea4431788f6025aa
Az Foxxo <contact@azfoxxo.com>
2025-08-17 04:45:20 +0100

    Added ReadInline to Heroes and bumped to newest dotnet version

--- Heroes/Attachables/HeroAttachableSystem.cs
+++ Heroes/Attachables/HeroAttachableSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Heroes.Attachables;
 
 public class HeroAttachableSystem : Common {
--- Heroes/Common.cs
+++ Heroes/Common.cs
@@ -1,4 +1,5 @@
 using Heroes.Attachables;
+using System;
 
 namespace Heroes;
 
@@ -42,6 +43,75 @@ public class Common
 
         // Return the input (check if the input is null or empty)
         return Console.ReadLine() ?? "";
+    }
+
+    /// <summary>
+    /// Read input from the user in a colour, with inline editing (arrow keys, insert, backspace).
+    /// </summary>
+    /// <param name="prompt">The prompt to display.</param>
+    /// <param name="colour">The colour to display the prompt in. Optional.</param>
+    /// <returns>The input from the user.</returns>
+    protected static string ReadInline(string prompt, Colours colour = default)
+    {
+        // Print the prompt with colour
+        Print(prompt, colour);
+
+        var input = new System.Text.StringBuilder();
+        int cursorPos = 0;
+
+        void RedrawInput()
+        {
+            int currentLine = Console.CursorTop;
+            int cursorLeft = prompt.Length;
+
+            Console.SetCursorPosition(cursorLeft, currentLine);
+            Console.Write(input.ToString() + " "); // Clear leftover characters
+            Console.SetCursorPosition(cursorLeft + cursorPos, currentLine);
+        }
+
+        while (true)
+        {
+            var keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                break;
+            }
+            else if (keyInfo.Key == ConsoleKey.LeftArrow)
+            {
+                if (cursorPos > 0)
+                {
+                    cursorPos--;
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.RightArrow)
+            {
+                if (cursorPos < input.Length)
+                {
+                    cursorPos++;
+                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace)
+            {
+                if (cursorPos > 0)
+                {
+                    cursorPos--;
+                    input.Remove(cursorPos, 1);
+                    RedrawInput();
+                }
+            }
+            else if (!char.IsControl(keyInfo.KeyChar))
+            {
+                input.Insert(cursorPos, keyInfo.KeyChar);
+                cursorPos++;
+                RedrawInput();
+            }
+        }
+
+        return input.ToString();
     }
 
     ///<summary>Read input from the user in a colour.</summary>
